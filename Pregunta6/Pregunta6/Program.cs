c8c2c70e3f49ebb3636bfd5d64d83421479b6ae2
KevinMero428 <56228157+KevinMero428@users.noreply.github.com>
2019-10-06 11:02:43 -0500

    Add files via upload

---
+++ Pregunta6/Pregunta6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar el valor de filas para el rombo:");
+            int fila = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= fila; i++)
+            {
+                for (int j = 1; j <= fila - i; j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+
+            for (int i = fila - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= fila - i; j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
