fddc4c7d8ec0afc27cb83cdadd86ab174ccb0a82
KevinMero428 <56228157+KevinMero428@users.noreply.github.com>
2019-10-06 11:03:24 -0500

    Add files via upload

---
+++ Pregunta7/Pregunta7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dos columnas de numero del 1 al 100 y del 100 al 1:");
+            int j = 100;
+            for (int i = 1; i <= j; i++)
+            {
+                Console.Write(i);
+                Console.Write("   ");
+                Console.WriteLine(101 - i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
