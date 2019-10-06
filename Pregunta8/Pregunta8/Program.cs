a4474f768cee3de91c336bbf4d754e2d7e7b01d8
KevinMero428 <56228157+KevinMero428@users.noreply.github.com>
2019-10-06 11:03:57 -0500

    Add files via upload

---
+++ Pregunta8/Pregunta8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresar numero entero para empezar un conteo:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int doble = 0;
+            int triple = 0;
+            for (int i = 1; i <= numero; i++)
+            {
+                Console.Write(i);
+                Console.Write(" ");
+
+                doble = doble + 2;
+                if(doble > numero )
+                {
+                    Console.Write("  ");
+                }
+                else
+                {
+                    Console.Write(doble);
+                }
+                Console.Write("  ");
+                triple = triple + 3;
+                if(triple > numero)
+                {
+                    Console.Write("  ");
+                }
+                else
+                {
+                    Console.Write(triple);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
