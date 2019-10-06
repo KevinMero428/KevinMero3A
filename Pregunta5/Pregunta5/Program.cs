021a13552ff65a2c69b4db86d393e46b806eda0b
KevinMero428 <56228157+KevinMero428@users.noreply.github.com>
2019-10-06 11:01:07 -0500

    Add files via upload

---
+++ Pregunta5/Pregunta5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+           Console.WriteLine("Ingresar un numero entero para crea la piramide");
+           int valor = Convert.ToInt32(Console.ReadLine());
+           int j = 0;
+           int k = 0;
+           for (int i = 1; i <= valor; i++)
+           {
+               for (j = 1; j <= (valor - i); j++)
+                    Console.Write(" ");
+               for (k = 1; k <= i; k++)
+                    Console.Write('*');
+               for (k = (i - 1); k >= 1; k--)
+                    Console.Write('*');
+                Console.WriteLine();
+           }
+           Console.ReadKey();         
+        }
+    }
+}
