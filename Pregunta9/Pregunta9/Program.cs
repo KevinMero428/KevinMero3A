4cc9f8e7080234a0f157963bbc7f422f7b57b1aa
KevinMero428 <56228157+KevinMero428@users.noreply.github.com>
2019-10-06 11:04:30 -0500

    Add files via upload

---
+++ Pregunta9/Pregunta9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Ingresar un numero para generar una tabla de multiplicar:");
+            string numero = Console.ReadLine();
+            int valor = 0;
+            bool error = Int32.TryParse(numero, out valor);
+            int i = 1;    
+            if (error)
+            {
+                for (i = 1; i <= 15; i++)
+                {
+                    Convert.ToInt32(numero);
+                    Console.WriteLine("{0} x {1} = {2}", valor, i, valor*i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("El valor ingresado no es correcto");
+            }
+            Console.ReadKey();
+        }
+    }
+}
