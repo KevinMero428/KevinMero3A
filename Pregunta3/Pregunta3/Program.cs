301a0701f92e35d7b0d886e1289e981dedc90dec
KevinMero428 <56228157+KevinMero428@users.noreply.github.com>
2019-10-06 10:59:48 -0500

    Add files via upload

---
+++ Pregunta3/Pregunta3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int producto = 0;
+            int valor = 0;
+            int acomulador = 0;
+            do
+            {
+                Console.WriteLine("Ingresar la cantidad del producto vendido:");
+                producto = Convert.ToInt32(Console.ReadLine());
+                if (producto != 0)
+                {
+                    Console.WriteLine("Ingresar el valor del producto comprado:");
+                    valor = Convert.ToInt32(Console.ReadLine());
+                }
+                acomulador = acomulador + (valor * producto);
+            } while (producto != 0);
+            Console.WriteLine("El valor a cancelar es: {0}", acomulador);
+            Console.ReadKey();
+        }
+    }
+}
