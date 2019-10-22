e762b8bfd6204a174fee0d038c5426412f55e073
KevinMero428 <56228157+KevinMero428@users.noreply.github.com>
2019-10-21 19:27:35 -0500

    Add files via upload

---
+++ Encapsulamiento/Encapsulamiento/Clases/Factura.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamiento.Clases
+{
+    public class Factura
+
+    {
+        string opcion;
+
+        private string cancha;
+
+        public string Cancha
+        {
+            get { return cancha; }
+            set { cancha = value; }
+
+        }
+
+        private float costo;
+
+        public float Costo
+        {
+            get
+            {
+                switch (cancha)
+                {
+                    case "1":
+                        opcion = "Futbol";
+                        costo = 20;
+                        break;
+
+                    case "2":
+                        opcion = "Fulbito";
+                        costo = 15;
+                        break;
+
+                    case "3":
+                        opcion = "Indor";
+                        costo = 5;
+                        break;
+
+                    case "4":
+                        opcion = "Basquet";
+                        costo = 5;
+                        break;
+
+                    default:
+                        Console.WriteLine("***Ingresar una opcion valida***");
+                        break;
+                }
+                return costo;
+            }
+        }
+
+        private float horas;
+
+        public float Horas
+        {
+            get { return horas; }
+            set
+            {
+
+                if (value > 3)
+                {
+                    throw new Exception("No puede solicitar mas de 3 horas");
+                }
+                horas = value;
+            }
+        }
+
+        private float subtotal;
+
+        public float Subtotal
+        {
+            get
+            {
+                subtotal = costo * horas;
+                return subtotal;
+            }
+        }
+
+        private float descuento;
+
+        public float Descuento
+        {
+            get { return descuento; }
+            set
+            {
+                if (value == 10 || value == 20 || value == 0)
+                {
+                    descuento = value;
+                }
+                else
+                    throw new Exception("No ingreso un descuento valido");
+            }
+        }
+
+        private float subtotal2;
+        public float Subtotal2
+        {
+
+            get
+            {
+                if (descuento == 10)
+                {
+                    subtotal2 = subtotal - ((subtotal * 10) / 100);
+                    return subtotal2;
+                }
+                else if (descuento == 20)
+                {
+                    subtotal2 = subtotal - ((subtotal * 20) / 100);
+                    return subtotal2;
+                }
+                subtotal2 = subtotal;
+                return subtotal2;
+            }
+        }
+
+        private float iva;
+
+        public float IVA
+        {
+            get
+            {
+                iva = (subtotal2 * 12) / 100;
+                return iva;
+            }
+        }
+
+        private float final;
+
+        public float Final
+        {
+            get
+            {
+                final = subtotal2 + iva;
+                return final;
+            }
+        }
+
+        private float pagado;
+
+        public float Pagado
+        {
+            get
+            {
+                float abonar = final / 2;
+                if (pagado >= abonar)
+                {
+                    return pagado;
+                }
+                else
+                    throw new Exception("Debe cancelar al menos el 50% del total a pagar");
+            }
+            set { pagado = value; }
+        }
+
+        private float pendiente;
+
+        public float Pendiente
+        {
+            get
+            {
+                pendiente = final - pagado;
+                return pendiente;
+            }
+        }
+
+        private string estado;
+
+        public string Estado
+        {
+            get
+            {
+
+                if (pendiente > 0)
+                {
+                    estado = "PENDIENTE";
+                    return estado;
+                }
+                else
+                {
+                    estado = "PAGADO";
+                    return estado;
+                }
+            }
+        }
+    }
+}
---
+++ Encapsulamiento/Encapsulamiento/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Encapsulamiento.Clases;
+
+namespace Encapsulamiento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factura factura = new Factura();
+
+            Program.menu();
+
+            string seleccion = Console.ReadLine();
+            factura.Cancha = seleccion;
+            
+
+
+            Console.WriteLine("Ingresar la cantidad de horas:");
+            factura.Horas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingresar el descuento que desea aplicar:");
+            factura.Descuento = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingresar el valor a cancelar");
+            factura.Pagado = Convert.ToInt32(Console.ReadLine());
+            
+
+            //Console.WriteLine("Cancha: {0}", factura.Cancha);
+            Console.WriteLine("El costo de la cancha es: {0}", factura.Costo);
+            Console.WriteLine("Horas elegida: {0}", factura.Horas);
+            Console.WriteLine("Subtotal 1: {0}", factura.Subtotal);
+            Console.WriteLine("El descuento es de: {0}%", factura.Descuento);
+            Console.WriteLine("El subtotal con descuento es: {0}", factura.Subtotal2);
+            Console.WriteLine("El valor del IVA es: {0}", factura.IVA);
+            Console.WriteLine("El precio final es: {0}", factura.Final);
+            Console.WriteLine("El valor pagado es: {0}", factura.Pagado);
+            Console.WriteLine("El saldo pendiente es de: {0}", factura.Pendiente);
+            Console.WriteLine("ESTADO: {0}", factura.Estado);
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+        static void menu()
+        {
+           Console.WriteLine("MENU");
+           Console.WriteLine("1.- Futbol ----------- $20");
+           Console.WriteLine("2.- Fulbito ----------- $15");
+           Console.WriteLine("3.- Indor ----------- $5");
+           Console.WriteLine("4.- Basquet ----------- $5");
+
+        }
+    }
+
+}
